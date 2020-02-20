af90c4d536b541e5c43e3005ebccc06e5ae84269
Bartlomiej Puget <larhard@gmail.com>
2020-02-20 21:25:17 +0100

    Add ghc

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 LOCAL_HOME="$PWD/home"
-docker run --rm -it --name ghc_analyzer -v "$LOCAL_HOME:/root:rw" ghc_analyzer
+
+# IMAGE=ghc_analyzer
+# # DOCKER_HOME=/root
+# DOCKER_HOME=/home/ghc
+
+IMAGE=gregweber/ghc-haskell-dev
+DOCKER_HOME=/home/ghc
+
+docker run --rm -it --name ghc_analyzer -v "$LOCAL_HOME:$DOCKER_HOME:rw" "$IMAGE" /bin/bash
