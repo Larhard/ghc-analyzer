23f5869e361fac8bd85f034e4cb0bd1e00a0a6b1
Bartlomiej Puget <larhard@gmail.com>
2020-02-20 22:16:07 +0100

    Update

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ LOCAL_HOME="$PWD/home"
 # DOCKER_HOME=/home/ghc
 
 IMAGE=gregweber/ghc-haskell-dev
-DOCKER_HOME=/home/ghc
+DOCKER_HOME=/home/ghc/project
 
 docker run --rm -it --name ghc_analyzer -v "$LOCAL_HOME:$DOCKER_HOME:rw" "$IMAGE" /bin/bash
