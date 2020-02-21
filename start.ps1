1b96d4fc91dd0a8780f85380858cc9c0900e4d59
Puget Bartlomiej-HJQN67 <bartlomiej.puget@motorolasolutions.com>
2020-02-21 14:56:56 +0100

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,2 +1,10 @@
 $LOCAL_HOME = "$PWD\home" -replace "\\","/"
-docker run --rm -it --name ghc_analyzer -v "$LOCAL_HOME`:/root:rw" ghc_analyzer
+
+# $IMAGE=ghc_analyzer
+# # $DOCKER_HOME=/root
+# $DOCKER_HOME=/home/ghc
+
+$IMAGE = "gregweber/ghc-haskell-dev"
+$DOCKER_HOME = "/home/ghc/project"
+
+docker run --rm -it --name ghc_analyzer -v "$LOCAL_HOME`:$DOCKER_HOME`:rw" "$IMAGE"
